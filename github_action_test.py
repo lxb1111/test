94d302ba277eb18219f55a124e2b7eda5981477c
lixiaobiao <lixiaobiao@baidu.com>
2021-06-24 18:14:30 +0800

    test workfolow

---
+++ github_action_test.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+"""
+author  : lxb
+note    : test CI use github action
+time    : 2021/6/23 4:10 下午
+"""
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    """
+    test
+    """
+    def setUp(self):
+        """
+        case start
+        :return:
+        """
+        self.a = 1
+        self.b = 2
+
+    def test_case(self):
+        """
+        case run
+        :return:
+        """
+        c = self.a + self.b
+        self.assertEqual(3, c)
---
+++ start_deploy.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+author     : lxb
+note       : start_deploy.py
+time       : 2021/6/24 5:23 下午
+"""
+
+if __name__ == "__main__":
+    print('hello world')
+
+
