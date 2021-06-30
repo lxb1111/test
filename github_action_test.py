d17e2b3e021d3bc2a3ce190d9694ae42b051216c
lxb1111 <58289576+lxb1111@users.noreply.github.com>
2021-06-30 15:51:39 +0800

    Update github_action_test.py

--- github_action_test.py
+++ github_action_test.py
@@ -19,7 +19,15 @@ class TestCase(unittest.TestCase):
         self.a = 1
         self.b = 2
 
-    def test_case(self):
+    def test_case_1(self):
+        """
+        case run
+        :return:
+        """
+        c = self.a + self.b
+        self.assertEqual(3, c)
+    
+    def test_case_2(self):
         """
         case run
         :return:
